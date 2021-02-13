5a2342eb5c681277f3753f3d79803348c32df918
Garatee <kcliaomr@gmail.com>
2021-02-13 02:35:03 -0500

    added oracle logo for login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 /*global chrome*/
 
+import React from 'react';
 import './App.css';
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  doSomething() {
+    const key = "key87";
+    const value = "val87";
+    chrome.storage.sync.set({key: value}, function() {
+      console.log('sync Value is set to ' + value);
+    });
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <Logo src="logo/oracle-logo-bar.png" alt="oracle logo"/>
+        
+      </Wrapper>
+    )
+  }
+
 }
+
+const Wrapper = styled.div`
+  width: 600px;
+  height: 478px;
+  background-color: #f7f7f7;
+  box-shadow: 0 -1px 0 rgb(0 0 0 / 15%);
+  transition: bottom 0.4s ease 0s;
+`
+
+const Logo = styled.img`
+  width: 70%;
+  margin-left: 15%;
+  margin-right: 15%;
+`
+
 
 export default App;
